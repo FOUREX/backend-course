cc3c45419c6548f7d5b49b209abe143267fedfc1
Жеревчук Сергій <Foxtrotserega@gmail.com>
2025-01-21 10:40:08 +0100

    Set unique for users.email

---
+++ src/api/auth.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy.exc import IntegrityError
+from passlib.context import CryptContext
+
+from src.repositories.users import UsersRepository
+from src.database import async_session_maker
+from src.schemas.users import UserRequestAdd, UserAdd
+
+router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+@router.post("/register")
+async def register_user(
+        data: UserRequestAdd,
+):
+    hashed_password = pwd_context.hash(data.password)
+    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    async with async_session_maker() as session:
+        try:
+            await UsersRepository(session).add(new_user_data)
+            await session.commit()
+        except IntegrityError:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="This e-mail already in use"
+            )
+
+    return {"status": "OK"}
